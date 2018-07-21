302b17a90c7dfc5fd8d4923b26b1baa86104a382
Morten Ingemann <morteningemann86@gmail.com>
2018-07-21 14:29:24 +0200

    Adding existing files

---
+++ Module 1/helloScript.js
@@ -0,0 +1,15 @@
+var msg = 'Hello World!'
+window.alert(msg)
+console.log('Hello console!')
+document.write('Hello document!')
+
+/***********************************
+NEW CODE ATTEMPT - WORKING WITH JAVASCRIPT OBJECTS
+************************************/
+var book = {
+    title: "Romeo and Juliet",
+    author: "William Shakespeare",
+    publishDate: 1597
+};
+
+console.log(book.publishDate)
---
+++ Module 2/loops.js
@@ -0,0 +1,34 @@
+var count = 0
+while (count < 10) {
+  document.write('Hello world! ')
+  count++
+}
+
+var count2 = 1
+while (count2 < 11) {
+  document.write(count2 + ' ')
+  count2++
+}
+
+for (i = 1; i < 10; i++) {
+  document.write('Hey There ')
+}
+
+/**
+FOR LOOP:
+for(initialization, condition, modification)
+{
+    // code goes here
+    // do something
+}
+
+---------------------------------------------------
+
+WHILE LOOP:
+while(condition)
+{
+    //code to repeat
+   // do something here
+   // This code should modify the condition to eventually end the loop
+}
+*/
